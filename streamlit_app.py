87bdd455e48d109a2380b5b744287ff1b23f1569
Ayon1502112 <152854568+Ayon1502112@users.noreply.github.com>
2025-05-18 09:37:30 +0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ urls = {
     "Live Die Maintenance Report": "https://lookerstudio.google.com/reporting/6d689733-0d3c-4667-ac7f-ee96cd4f0523",
     "Hasan Vhai (VMC Entry)": "https://www.appsheet.com/start/8c16d536-adb4-4cdd-a1bb-e4a66e6753b2",
     "Hasan Vhai (VMC Cutter Dashboard)": "https://lookerstudio.google.com/reporting/6556c179-2249-4fcf-9a52-5d8fbff20e3c",
-    "Job List Die Modification/Service": "https://www.appsheet.com/start/55d59741-2ae3-41a7-8d18-cc47a5775272"   
+    "Maintenance Monitoring (Entry Form)": "https://www.appsheet.com/start/a81e8891-6548-4994-80c5-d7ec4f7d8c29",
+    "Job List Die Modification/Service": "https://www.appsheet.com/start/55d59741-2ae3-41a7-8d18-cc47a5775272",
+    "Service": "https://lookerstudio.google.com/reporting/2b600a29-9938-4810-96db-a60eb99574ec"
 }
 
 # Create buttons using st.link_button()
